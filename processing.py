2ce0be04836e5e7356a48cf68e1d754e933f84b6
Zachary Noah <zacharybnoah@gmail.com>
2019-02-20 12:33:59 -0800

    switching over to SQL alchemy for bulk insert

--- processing.py
+++ processing.py
@@ -21,7 +21,7 @@ for stops_row in stops_table:
 
 #This gets all the vehicles and puts them into an array of objects.
 def populate_vehicles(cur):
-    cur.execute("SELECT * FROM vehicles;")
+    cur.execute("SELECT * FROM vehiclesubset LIMIT 1000;")
     vehicles = []
     vehicles_table = cur.fetchall()
     for vehicles_row in vehicles_table:
@@ -29,26 +29,26 @@ def populate_vehicles(cur):
         vehicles.append(vehicle)
     return vehicles
 
-#This is confusing with get_stops_for_route, need to rename to be less confusing 
+#This is confusing with get_stops_for_route, need to rename to be less confusing
 def get_stops_on_route(cur, vehicle, stops):
     stops_on_route = []
     for stop in stops:
-        
+
         for route in stop.json_routes:
             if vehicle.route_id == route:
                 stops_on_route.append(stop)
 
-    
+
     return stops_on_route
-            
+
 
 def get_distance_from_stop(cur, vehicle, stop):
-   
+
     vehicle_point = wkb.loads(vehicle.loc, hex=True)
     stop_point = wkb.loads(stop.geom, hex=True)
     distance = vincenty((vehicle_point.y, vehicle_point.x), (stop_point.y, stop_point.x)).m
     return distance
-    
+
 def get_closest_stop_on_route(vehicle):
     stop_dist_dict = {vehicle: {}}
     distance_list = []
@@ -80,46 +80,65 @@ multiple_insert_template_stirng = "INSERT INTO stoppedvehicles (id, trip_id, sta
 limiter = 0
 counter = 0
 for vehicle in vehicles:
-    print("stop check called!")
-    if vehicle.speed == "0.0":
-        stop_dist_dict = get_closest_stop_on_route(vehicle)
-        # closest_stop is stop_id and closest_stop[1] is distance
-        # if stop_dist_dict.items() == {}:
-        #     continue
+    # print("stop check called!")
+    # if vehicle.speed == "0.0":
+    #     stop_dist_dict = get_closest_stop_on_route(vehicle)
+    #
+    #     if len(stop_dist_dict[vehicle].items()) == 0:
+    #         continue
+    #     closest_stop = min(stop_dist_dict[vehicle].items(), key=lambda kv: kv[1])
+    #     # print("closest stop....", closest_stop)
+    #     stop_id = closest_stop[0]
+    #     if closest_stop[1] < 25:
+    #
+    #         print("vehicle timestmap?", vehicle.vehicle_id)
+    #         print("python datatype for timestamp field", type(vehicle.timestamp))
+    #
+    #         sql_bulk_insert = """
+    #          ('{id}', '{trip_id}', '{start_time}', '{start_date}',
+    #                                     '{route_id}', {stop_id}, '{loc}',
+    #                                     '{bearing}', '{speed}', '{timestamp}', '{vehicle_id}'),
+    #         """.format(
+    #             id=vehicle.id,
+    #             trip_id=vehicle.trip_id,
+    #             start_time=vehicle.start_time,
+    #             start_date=vehicle.start_date,
+    #             route_id=vehicle.route_id,
+    #             stop_id=stop_id,
+    #             loc=vehicle.loc,
+    #             bearing=vehicle.bearing,
+    #             speed=vehicle.speed,
+    #             timestamp=vehicle.timestamp,
+    #             vehicle_id=vehicle.vehicle_id
+    #         )
+    #         print(counter)
+    #         counter += 1
+    #         multiple_insert_template_stirng += sql_bulk_insert
 
-        if len(stop_dist_dict[vehicle].items()) == 0:
-            continue
-        closest_stop = min(stop_dist_dict[vehicle].items(), key=lambda kv: kv[1])
-        # print("closest stop....", closest_stop)
-        stop_id = closest_stop[0]
-        if closest_stop[1] < 25:
 
-            print("vehicle timestmap?", vehicle.vehicle_id)
-            print("python datatype for timestamp field", type(vehicle.timestamp))
+    sql_bulk_insert = """
+     ('{id}', '{trip_id}', '{start_time}', '{start_date}',
+                                '{route_id}', {stop_id}, '{loc}',
+                                '{bearing}', '{speed}', '{timestamp}', '{vehicle_id}'),
+    """.format(
+        id=vehicle.id,
+        trip_id=vehicle.trip_id,
+        start_time=vehicle.start_time,
+        start_date=vehicle.start_date,
+        route_id=vehicle.route_id,
+        stop_id=stop_id,
+        loc=vehicle.loc,
+        bearing=vehicle.bearing,
+        speed=vehicle.speed,
+        timestamp=vehicle.timestamp,
+        vehicle_id=vehicle.vehicle_id
+    )
 
-            sql_bulk_insert = """
-             ('{id}', '{trip_id}', '{start_time}', '{start_date}',
-                                        '{route_id}', {stop_id}, '{loc}', 
-                                        '{bearing}', '{speed}', '{timestamp}', '{vehicle_id}'),
-            """.format(
-                id=vehicle.id,
-                trip_id=vehicle.trip_id,
-                start_time=vehicle.start_time,
-                start_date=vehicle.start_date,
-                route_id=vehicle.route_id,
-                stop_id=stop_id,
-                loc=vehicle.loc,
-                bearing=vehicle.bearing,
-                speed=vehicle.speed,
-                timestamp=vehicle.timestamp,
-                vehicle_id=vehicle.vehicle_id
-            )
-            print(counter)
-            counter += 1
-            multiple_insert_template_stirng += sql_bulk_insert
-    
+    multiple_insert_template_stirng += sql_bulk_insert
+
+
     limiter += 1
-    if limiter > 800:
+    if counter > 5:
         multiple_insert_template_stirng = multiple_insert_template_stirng[:-14] + ';'
         multiple_insert_template_stirng = multiple_insert_template_stirng.replace("None", "")
         print("the increasing big bulk import statement....")
@@ -127,6 +146,7 @@ for vehicle in vehicles:
         cur.execute(multiple_insert_template_stirng)
         multiple_insert_template_stirng = "INSERT INTO stoppedvehicles (id, trip_id, start_time, start_date, route_name, stop_id, loc, bearing, speed, vehicle_timestamp, vehicle_id) VALUES"
         limiter = 0
+        break
 
 
 
@@ -148,7 +168,7 @@ for vehicle in vehicles:
 
     #         sql_bulk_insert = """
     #          ('{id}', '{trip_id}', '{start_time}', '{start_date}',
-    #                                     '{route_id}', {stop_id}, '{loc}', 
+    #                                     '{route_id}', {stop_id}, '{loc}',
     #                                     '{bearing}', '{speed}', '{timestamp}', '{vehicle_id}'),
 
     #         """.format(
@@ -164,11 +184,11 @@ for vehicle in vehicles:
     #             timestamp=vehicle.timestamp,
     #             vehicle_id=vehicle.vehicle_id
     #         )
-            
+
     #         multiple_insert_template_stirng = multiple_insert_template_stirng + sql_bulk_insert
     #         # sql_string = """
     #         # INSERT INTO stoppedvehicles VALUES('{id}', '{trip_id}', '{start_time}', '{start_date}',
-    #         #                             '{route_id}', {stop_id}, '{loc}', 
+    #         #                             '{route_id}', {stop_id}, '{loc}',
     #         #                             '{bearing}', '{speed}', '{timestamp}', '{vehicle_id}')
 
     #         # """.format(
@@ -185,12 +205,12 @@ for vehicle in vehicles:
     #         #     vehicle_id=vehicle.vehicle_id
     #         # )
     #         # print(sql_string)
-            
+
     #         # cur.execute(sql_string)
     #         # print("unpack closest stop", closest_stop)
     #         # print("stop dict", stop_dist_dict)
     #         # print("closest stop", closest_stop)
-            
+
     #     else:
     #         print("need moar data")
     #         continue
@@ -208,7 +228,7 @@ for vehicle in vehicles:
 #         distance_list.append(get_distance_from_stop(cur, vehicle, stop))
 #         vehicle_map[vehicle] = min(distance_list)
 #         # print("distance stop<-->vehicle", dist)
-        
+
 #     sorted_by_value = sorted(vehicle_map.items(), key=lambda kv: kv[1])
 #     print(sorted_by_value)
 
@@ -230,4 +250,3 @@ for vehicle in vehicles:
 #         INSERT INTO sanpablorapidstops (SELECT * FROM stops WHERE stop_id={})
 #     """.format(stop.stop_id)
 #     cur.execute(sql_string)
-
